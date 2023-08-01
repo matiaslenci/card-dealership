69df2c74ae5aa9b87e820ede3a2200cf259b4a1e
Matias Lenci <matiaslenci@gmail.com>
2023-08-01 20:58:28 -0300

    Post,Patch y delete

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -15,5 +24,43 @@ export class CarsController {
   getCarById(@Param('id', ParseIntPipe) id: number) {
     //Retorna el auto que coincida el id con la posicion del array
     return this.carsSrv.findCardByid(id);
+  }
+
+  /**
+   * Crea un nuevo auto
+   * @param body decorador especial de Nest para manipular el body
+   * de nuestra req
+   * @returns el objeto auto que creamos
+   */
+  @Post()
+  createCar(@Body() body: any) {
+    return body;
+  }
+
+  /**
+   * Actualiza los valores del auto que especifiquemos
+   * espera un id como parametro
+   * @returns el auto actualizado
+   */
+  @Patch(':id')
+  updateCar(
+    @Param('id', ParseIntPipe)
+    @Body()
+    body: any,
+  ) {
+    return body;
+  }
+
+  /**
+   * elimina un auto
+   * @param id del auto que se quiere eliminar
+   * @returns el id del objeto eliminado
+   */
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'delete',
+      id,
+    };
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ICar } from './interfaces/cars.interfaces';
 
 @Injectable()
