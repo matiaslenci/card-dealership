4246222e6e245448cbba854857db1483d161b09a
Matias Lenci <matiaslenci@gmail.com>
2023-08-05 14:49:03 -0300

    CRUD de brands

---
+++ src/brands/entities/brand.entity.ts
@@ -0,0 +1,10 @@
+//Como se ve mi tabla en la db
+export class Brand {
+  id: string;
+  name: string;
+
+  //fecha de cuando fue creado
+  createdAt: number;
+  //fecha de cuando fue actualizado
+  updatedAt?: number;
+}
