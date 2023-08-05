b966a82bf25984affe842cc0189cb6bc2d918d56
Matias Lenci <matiaslenci@gmail.com>
2023-08-05 19:23:40 -0300

    mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { BrandsModule } from './brands/brands.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CarsModule, BrandsModule],
+  imports: [
+    CarsModule,
+    BrandsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-cars'),
+  ],
   controllers: [],
   providers: [],
   exports: [],
